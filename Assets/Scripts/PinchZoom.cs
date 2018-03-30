936dead098c0d46970b22a2d83bb90b995ad28e9
Everton Coelho <evertonscoelho@gmail.com>
2018-03-30 00:13:16 -0300

    Zoom camera touch

---
+++ Assets/Scripts/PinchZoom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PinchZoom : MonoBehaviour
+{
+    public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+    public float orthoZoomSpeed = 0.01f;        // The rate of change of the orthographic size in orthographic mode.
+
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // ... change the orthographic size based on the change in distance between the touches.
+            Camera camera = GetComponent<Camera>();
+            camera.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+            // Make sure the orthographic size never drops below zero.
+            camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.5f);
+
+            camera.orthographicSize = Mathf.Min(camera.orthographicSize, 20f);
+        }
+    }
+}
