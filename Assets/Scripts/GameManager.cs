3a4c9ad99bf77ad9a9fbde604201cb8f89fa7f4a
Everton Coelho <evertonscoelho@gmail.com>
2018-04-03 01:02:37 -0300

    changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;       //Allows us to use Lists. 
+using System.Collections.Generic;       //Allows us to use Lists.
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
     private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
-    private int level = 3;                                  //Current level number, expressed in game as "Day 1".
 
     //Awake is always called before any Start functions
     void Awake()
@@ -38,14 +37,6 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         //Call the SetupScene function of the BoardManager script, pass it current level number.
-        boardScript.SetupScene(level);
-    }
-
-
-
-    //Update is called every frame.
-    void Update()
-    {
-
+    
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Level
+{
+    public string name;
+    public string levelID;
+    public string author;
+    public int difficulty;
+    public Board board;
+
+}
+
+[System.Serializable]
+public class Board
+{
+    public string fields;
+    public DataBoard[] data;
+}
+
+[System.Serializable]
+public class DataBoard
+{
+    public int positionX;
+    public int positionY;
+    public string objectType;
+}
